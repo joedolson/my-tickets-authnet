2b2dd408b82bcf12a2a0cc6b2aa5d71be2ea02d9
Joe Dolson <design@joedolson.com>
2018-04-22 15:10:51 -0500

    Update to new header; version numbers

--- AuthorizeNet.php
+++ AuthorizeNet.php
@@ -1,30 +1,45 @@
 <?php
+/**
+ * My Tickets: Stripe payment gateway
+ *
+ * @package     My Tickets: Authorize.net
+ * @author      Joe Dolson
+ * @copyright   2014-2018 Joe Dolson
+ * @license     GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: My Tickets: Authorize.net
+ * Plugin URI: http://www.joedolson.com/my-tickets/add-ons/
+ * Description: Add support for the Authorize.net payment gateway to My Tickets.
+ * Author: Joseph C Dolson
+ * Author URI: http://www.joedolson.com
+ * Text Domain: my-tickets-authnet
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/license/gpl-2.0.txt
+ * Domain Path: lang
+ * Version:     1.1.4
+ */
+
 /*
-Plugin Name: My Tickets: Authorize.net
-Plugin URI: http://www.joedolson.com/
-Description: Add support for the Authorize.net payment gateway to My Tickets.
-Author: Joseph C Dolson
-Author URI: http://www.joedolson.com/my-tickets-authnet/authorizenet
-Version: 1.1.3
+	Copyright 2014-2018  Joe Dolson (email : joe@joedolson.com)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-/*  Copyright 2014-2018  Joe Dolson (email : joe@joedolson.com)
 
-    This program is open source software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
 global $amt_version;
-$amt_version = '1.1.3';
+$amt_version = '1.1.4';
 
 load_plugin_textdomain( 'my-tickets-authnet', false, dirname( plugin_basename( __FILE__ ) ) . '/lang' );
 
